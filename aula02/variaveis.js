b5cf18d583c82dab7c4ab7cc030a515275eabb81
Adriano Takara <adrianotakara07@gmail.com>
2024-02-29 08:35:20 -0300

    Aula 02 sobre variaveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,16 @@
+// Exemplo de comentário
+
+var number = 17;
+let message = 'Text';
+const whatIsThat = '?';
+
+console.log(number);
+console.log(message);
+
+message = 100;
+
+console.log(message + number);
+
+console.log(whatIsThat);
+// Não é possível atribuir um novo valor em uma variável constante
+// whatIsThat = false;
